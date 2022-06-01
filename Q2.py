f8b89f34a450f513fa387189018a87e91a63fb9f
Anjali Bhardwaj <30476016+Anjalibhardwaj1@users.noreply.github.com>
2022-06-01 12:50:54 -0400

    q2

---
+++ Q2.py
@@ -0,0 +1,32 @@
+##!!!!!!!!!!! THIS QUESTION IS NOT DONE, READ EMAIL FROM TA
+# part a
+def lucas(n):
+	a,b = 2,1
+	if n < 0:
+		print("please enter a vaild number")
+	elif n == 0:
+		return a
+	elif n == 1:
+		return b
+	else:
+		for i in range(2,n+1):
+			a, b = b, a + b
+		return b
+
+def cal_lucas(n):
+    i=0
+    while i <= n:
+        print(lucas(i), end=' ')
+        i=i+1
+    print()
+
+# part b
+
+def lucas(n):
+	a, b = 2, 1
+	for i in range(n):
+	    yield a
+	    a, b = b, a + b
+
+def cal_lucas(n):
+    print(list(lucas(n)))
---
+++ Q4.py
@@ -0,0 +1,6 @@
+#Adding an element to the set
+#Removing all occurrances of an element from a set
+#determining the multiplicity of an element in the set
+#union of the set with another set: max multiplicity per element, new set is returned
+#intersection of the set with another set: min multiplicity per element
+#difference update: removing elements of another set from the current set and updateing the current set

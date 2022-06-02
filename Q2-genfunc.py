09664f3bdff9fbbe3cb9d358942701e0a937fa49
Rao Mohammad Mobeen-Ul-Haq <raomobeen@hotmail.co.uk>
2022-06-02 01:28:20 -0400

    splits and fixes Q2

---
+++ Q2-genfunc.py
@@ -0,0 +1,14 @@
+# part b
+
+def lucas(n):
+	a, b = 2, 1
+	for i in range(n):
+	    yield a
+	    a, b = b, a + b
+
+def cal_lucas(n):
+    #print(list(lucas(n)))
+    for num in lucas(n):
+        print(num, end=' ')
+
+#cal_lucas(20)
